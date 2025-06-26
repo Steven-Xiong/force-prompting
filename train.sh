f55bc8c3024b55c254a861579180e382d375654c
Steven-Xiong <x.zhexiao@wustl.edu>
2025-06-26 01:44:13 -0500

    6.25

---
+++ train.sh
@@ -0,0 +1,9 @@
+# bash scripts/train_4_gpu.sh \
+#     --force_type "point_force" \
+#     --video_root_dir "datasets/point-force/train/point_force_23000" \
+#     --csv_path "datasets/point-force/train/point_force_23000.csv"
+
+bash scripts/train_4_gpu.sh \
+    --force_type "wind_force" \
+    --video_root_dir "datasets/wind-force/train/wind_force_15359" \
+    --csv_path "datasets/wind-force/train/wind_force_15359.csv"
